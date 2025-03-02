66f8bd5cf23854f0d6ea77e4e3f7b551b1a9fb69
pantsel <panagis.tselentis@konghq.com>
2025-03-03 00:10:29 +0100

    wip

---
+++ scripts/gcloud/setup.sh
@@ -0,0 +1,12 @@
+set -e
+
+REGION=europe-west4
+VAULT_STORAGE_BUCKET=kpo-vault-storage
+
+gcloud services enable run.googleapis.com \
+    cloudbuild.googleapis.com \
+    secretmanager.googleapis.com \
+    sqladmin.googleapis.com \
+    iam.googleapis.com
+
+gsutil mb -l $REGION gs://$VAULT_STORAGE_BUCKET/
